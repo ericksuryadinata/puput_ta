ebc9ebfc72ff39d0d61968b6de400509647e4c4a
Erick Surya <ericksuryadinata@gmail.com>
2018-05-27 16:23:58 +0700

    change frontend website

--- application/views/website/landing-page/layouts/content.blade.php
+++ application/views/website/landing-page/layouts/content.blade.php
@@ -1,3 +1,4 @@
+
 @include('website.landing-page.layouts.navbar')
 
 @yield('content')
--- application/views/website/landing-page/layouts/header.blade.php
+++ application/views/website/landing-page/layouts/header.blade.php
@@ -2,8 +2,46 @@
 <div class="fullwidth-section">
     <div class="fullwidth-bg">
         <div class="content-full-width">
-            <div class="column dt-sc-one-fourth">
-                <h2 class="hr-border-title center"><span>Berita Terbaru</span></h2>
+            <div class="column dt-sc-one-fourth first" id="">
+                <div class="column dt-sc-one-column">
+                    <div class="dt-sc-titled-box-content-reset">
+                        <div class="dt-sc-titled-box  chocolate">
+                            <h6 class="dt-sc-titled-box-title">
+                                <span class="fa fa-graduation-cap"></span> Pendaftaran Mahasiswa Baru
+                            </h6>
+                            <div class="dt-sc-titled-box-content">
+                                Alur & Biaya Pendaftaran Mahasiswa Baru Teknik Informatika Untag Surabaya
+                                <a class="dt-sc-button  small  ocean"  href="statis-75-pmb.html" target="_blank">PMB</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column dt-sc-one-column">
+                    <div class="dt-sc-titled-box-content-reset">
+                        <div class="dt-sc-titled-box  chocolate">
+                            <h6 class="dt-sc-titled-box-title">
+                                <span class="fa fa-book"></span> Kurikulum Akademik
+                            </h6>
+                            <div class="dt-sc-titled-box-content">
+                                Mata Kuliah yang diajarkan di Teknik Informatika Untag Surabaya
+                                <a class="dt-sc-button small ocean"  href="#" target="_blank">Kurikulum</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column dt-sc-one-column">
+                    <div class="dt-sc-titled-box-content-reset">
+                        <div class="dt-sc-titled-box  chocolate">
+                            <h6 class="dt-sc-titled-box-title">
+                                <span class="fa fa-file"></span> Hasil Karya
+                            </h6>
+                            <div class="dt-sc-titled-box-content">
+                                Hasil Karya Mahasiswa & Dosen Teknik Informatika Untag Surabaya
+                                <a class="dt-sc-button small ocean" href="#" target="_blank">Hasil Karya</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="column dt-sc-one-half">
                 <div id="slider">
@@ -11,19 +49,19 @@
                         <div id="rev_slider_24_1" class="rev_slider fullscreenbanner" style="display:none;">
                             <ul>	
                                 <li class="rs-slide1" data-transition="random">
-                                    <img src="{{base_url('uploads/images/slider/IMG_9919.JPG')}}" alt="bg1" data-bgrepeat="no-repeat" data-bgfit="contain" data-bgposition="center center">
+                                    <img src="{{base_url('uploads/images/slider/IMG_9919.JPG')}}" alt="bg1" style="object-fit:cover">
                                 </li> 
                 
                                 <li class="rs-slide1" data-transition="random" >
-                                    <img src="{{base_url('uploads/images/slider/sdf2.jpg')}}" alt="bg2" data-bgrepeat="no-repeat" data-bgfit="contain" data-bgposition="center center">
+                                    <img src="{{base_url('uploads/images/slider/sdf2.jpg')}}" alt="bg2" style="object-fit:cover">
                                 </li> 
                 
                                 <li class="rs-slide1" data-transition="random" >
-                                    <img src="{{base_url('uploads/images/slider/robot.jpg')}}" alt="bg3" data-bgrepeat="no-repeat" data-bgfit="contain" data-bgposition="center center">
+                                    <img src="{{base_url('uploads/images/slider/robot.jpg')}}" alt="bg3" style="object-fit:cover">
                                 </li> 
                 
                                 <li class="rs-slide1" data-transition="random" >
-                                    <img src="{{base_url('uploads/images/slider/ramadhan2_-_Copy.jpg')}}" alt="bg4" data-bgrepeat="no-repeat" data-bgfit="contain" data-bgposition="center center">
+                                    <img src="{{base_url('uploads/images/slider/ramadhan2_-_Copy.jpg')}}" alt="bg4" style="object-fit:cover">
                                 </li> 
                             </ul>
                 
@@ -34,7 +72,13 @@
                 </div>                    
             </div>
             <div class="column dt-sc-one-fourth">
-                <h2 class="hr-border-title center"><span>Berita Terbaru</span></h2>
+                <div class="side-nav-container">
+                    <ul>
+                        <li>Login</li>
+                        <li>Staf Site</li>
+                        <li>Berita</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
