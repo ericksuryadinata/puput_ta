07d15aa6d5c42676e2006b6137d8647d25ae1e4a
Erick Surya <ericksuryadinata@gmail.com>
2018-07-06 18:17:04 +0700

    change loadspeed

--- application/views/website/landing-page/includes/styles.blade.php
+++ application/views/website/landing-page/includes/styles.blade.php
@@ -1,20 +1,20 @@
-<link id="default-css" rel="stylesheet" href="{{base_url('assets/website/style.css')}}" type="text/css">
-<link id="shortcodes-css" rel="stylesheet" href="{{base_url('assets/website/css/shortcodes.css')}}" type="text/css">
-<link id="responsive-css" rel="stylesheet" href="{{base_url('assets/website/css/responsive.css')}}" type="text/css">
-<link id="skin-css" rel="stylesheet" href="{{base_url('assets/website/skins/electricblue/style.css')}}" type="text/css">
+<link id="default-css" rel="stylesheet" href="{{base_url('assets/website/style.min.css')}}" type="text/css">
+<link id="shortcodes-css" rel="stylesheet" href="{{base_url('assets/website/css/shortcodes.min.css')}}" type="text/css">
+<link id="responsive-css" rel="stylesheet" href="{{base_url('assets/website/css/responsive.min.css')}}" type="text/css">
+<link id="skin-css" rel="stylesheet" href="{{base_url('assets/website/skins/electricblue/style.min.css')}}" type="text/css">
 {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css" type="text/css"> --}}
 
 <!-- Additional Stylesheets -->    
-<link rel="stylesheet" href="{{base_url('assets/website/css/component.css')}}" type="text/css">
-<link href="{{base_url('assets/website/css/pace-theme-loading-bar.css')}}" rel="stylesheet" media="all" />    
+<link rel="stylesheet" href="{{base_url('assets/website/css/component.min.css')}}" type="text/css">
+<link href="{{base_url('assets/website/css/pace-theme-loading-bar.min.css')}}" rel="stylesheet" media="all" />    
 <!-- Google fonts -->  
 <link href='https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
 <!-- Font Awesome -->
-<!-- <link rel="stylesheet" href="{{base_url('assets/website/css/font-awesome.min.css')}}" type="text/css"> -->
-<link rel="stylesheet" href="{{base_url('assets/website/css/font-awesome.css')}}" type="text/css">
+<link rel="stylesheet" href="{{base_url('assets/website/css/font-awesome.min.css')}}" type="text/css">
+{{-- <link rel="stylesheet" href="{{base_url('assets/website/css/font-awesome.css')}}" type="text/css"> --}}
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 
-<link rel="stylesheet" id="rs-plugin-settings-css"  href="{{base_url('assets/website/css/settings.css')}}" type="text/css" media="all" />
+<link rel="stylesheet" id="rs-plugin-settings-css"  href="{{base_url('assets/website/css/settings.min.css')}}" type="text/css" media="all" />
 @yield('additional-styles')
